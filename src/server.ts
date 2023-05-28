bd52d070874fb415c2ace6e05bca9bdb92163479
Ashikur Rahman <ashikur.rahman9997@gmail.com>
2023-05-29 01:22:12 +0600

    eslint husky prettier and lintstage is configured

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import app from "./App";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './App'
+import config from './config'
 
 async function Connect() {
   try {
-    await mongoose.connect(config.mongoURI as string);
-    console.log("Database connection established");
+    await mongoose.connect(config.mongoURI as string)
+    console.log('Database connection established')
     app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port}`);
-    });
+      console.log(`Server running on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
-Connect();
+Connect()
